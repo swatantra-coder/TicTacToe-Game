b9caffcda5ee93f83498a6d29f22f5f3edacde94
Swatantra Singh <68724074+swatantra-coder@users.noreply.github.com>
2024-11-22 22:25:29 +0530

    Delete LLD-Tic-Tac-Toe directory

--- LLD-Tic-Tac-Toe/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        TicTacToeGame game = new TicTacToeGame();
-        System.out.println("Game Winner is: " + game.startGame());
-    }
-}
